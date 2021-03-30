8c1ae288bf1ebb9e6579382b4980ac68b2a845f7
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-03-30 15:25:21 -0400

    feat: Add a draft of accessing the spreadsheet in __main__.py.

--- meSMSage/meSMSage/__main__.py
+++ meSMSage/meSMSage/__main__.py
@@ -1,5 +1,9 @@
 """Define the command-line interface for the meSMSage program."""
 
+from sheetfu import SpreadsheetApp
+
+import gspread
+import pandas
 import typer
 
 
@@ -11,6 +15,15 @@ def main(
     typer.echo("")
     typer.echo(f"The chosen Google Sheet is {sheet}!")
     typer.echo("")
+    # create a connection to the specified Google Sheet
+    google_client = gspread.service_account("mesmsage-59eaa7a7970e.json")
+    worksheet = google_client.open(sheet).sheet1
+    # create a Pandas dataframe from the spreadsheet's worksheet
+    dataframe = pandas.DataFrame(worksheet.get_all_records())
+    print(dataframe)
+
+    # sa = SpreadsheetApp('mesmsage-59eaa7a7970e.json')
+    # spreadsheet = sa.open_by_id('1ujmuIJy1NPhasIF4tnRSae0033hwa9g0wul5Ii2NwZU')
 
 
 if __name__ == "__main__":
