cef0e89e1ad876b1f0bd689b4ba5de3e7ee272b4
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-03-30 15:26:46 -0400

    fix: Remove commented out code from sheetfu in __main__.py.

--- meSMSage/meSMSage/__main__.py
+++ meSMSage/meSMSage/__main__.py
@@ -16,14 +16,12 @@ def main(
     typer.echo(f"The chosen Google Sheet is {sheet}!")
     typer.echo("")
     # create a connection to the specified Google Sheet
-    google_client = gspread.service_account("mesmsage-59eaa7a7970e.json")
+    google_client = gspread.service_account("credentials.json")
     worksheet = google_client.open(sheet).sheet1
     # create a Pandas dataframe from the spreadsheet's worksheet
     dataframe = pandas.DataFrame(worksheet.get_all_records())
     print(dataframe)
-
-    # sa = SpreadsheetApp('mesmsage-59eaa7a7970e.json')
-    # spreadsheet = sa.open_by_id('1ujmuIJy1NPhasIF4tnRSae0033hwa9g0wul5Ii2NwZU')
+    print()
 
 
 if __name__ == "__main__":
