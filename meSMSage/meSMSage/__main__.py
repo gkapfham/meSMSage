6770371d3bfdff5a64238969a49397bd04f42828
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-03-30 17:51:34 -0400

    refactor: Revise __main__ so that it calls the sheet.connect function.

--- meSMSage/meSMSage/__main__.py
+++ meSMSage/meSMSage/__main__.py
@@ -1,14 +1,14 @@
 """Define the command-line interface for the meSMSage program."""
 
 import demonstrate
+import sheet
 
-import gspread
 import pandas
 import typer
 
 
 def main(
-    sheet: str = typer.Option(...),
+    googlesheet: str = typer.Option(...),
 ):
     """Access the command-line argument(s) and then perform actions."""
     # display the debugging output for the program's command-line arguments
@@ -16,8 +16,7 @@ def main(
     typer.echo(f"The chosen Google Sheet is {sheet}!")
     typer.echo("")
     # create a connection to the specified Google Sheet
-    google_client = gspread.service_account("credentials.json")
-    worksheet = google_client.open(sheet).sheet1
+    worksheet = sheet.connect(googlesheet)
     # create a Pandas volunteers_dataframe from the spreadsheet's worksheet
     volunteers_dataframe = pandas.DataFrame(worksheet.get_all_records())
     demonstrate.demonstrate_pandas_analysis(volunteers_dataframe)
