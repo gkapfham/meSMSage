1911bcf60ce969134d7931f3eea31f58bbfd8210
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-03-31 13:42:30 -0400

    debug: Add debugging statement to show environment variable in sheets.

--- meSMSage/meSMSage/sheets.py
+++ meSMSage/meSMSage/sheets.py
@@ -1,8 +1,12 @@
 """Connect to a Google Sheet using gspread to access sheets at data frames."""
 
+import os
+
 from dotenv import load_dotenv
 from sheetfu import model
 from sheetfu import SpreadsheetApp
+
+import configure
 
 
 SHEET = "Sheet1"
@@ -10,7 +14,9 @@ SHEET = "Sheet1"
 
 def connect_to_sheet(requested_spreadsheet_id: str, requested_sheet_name: str = SHEET) -> model.Sheet:
     """Connect to the specified Google Sheet and return the requested sheet (default is "Sheet1")."""
+    logger = configure.configure_logging()
     load_dotenv()
+    logger.debug(f"Environment: {os.getenv('SHEETFU_CONFIG_TYPE')}")
     sa = SpreadsheetApp(from_env=True)
     spreadsheet = sa.open_by_id(requested_spreadsheet_id)
     sheet = spreadsheet.get_sheet_by_name(requested_sheet_name)
