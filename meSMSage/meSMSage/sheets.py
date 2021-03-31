0e873c07c8071eeaf5e12db36c16653529fcaa0f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-03-31 19:38:09 -0400

    feat: Add the extract_dataframe function to the sheets module.

--- meSMSage/meSMSage/sheets.py
+++ meSMSage/meSMSage/sheets.py
@@ -1,5 +1,7 @@
 """Connect to a Google Sheet using gspread to access sheets at data frames."""
 
+import pandas
+
 from dotenv import load_dotenv
 from sheetfu import model
 from sheetfu import SpreadsheetApp
@@ -26,3 +28,11 @@ def connect_to_sheet(
     logger.debug(sheet)
     logger.debug(type(sheet))
     return sheet
+
+
+def extract_dataframe(sheet: model.Sheet) -> pandas.DataFrame:
+    """Extract a Pandas DataFrame from the sheet in sheetfu's internal format."""
+    data_range = sheet.get_data_range()
+    values = data_range.get_values()
+    extracted_dataframe = pandas.DataFrame(values[1 : len(values)], columns=values[0])
+    return extracted_dataframe
