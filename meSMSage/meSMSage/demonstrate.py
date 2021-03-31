ba2ea85762d857ee506bc2f90c0035ecd1bd0063
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-03-31 15:43:51 -0400

    refactor: Use logger.debug instead of print in the demonstrate module.

--- meSMSage/meSMSage/demonstrate.py
+++ meSMSage/meSMSage/demonstrate.py
@@ -3,36 +3,33 @@
 import numpy
 import pandas
 
+import configure
+
 
 def demonstrate_pandas_analysis(volunteers_dataframe: pandas.DataFrame):
     """Demonstrate the use of different functions for the analysis of a pandas data frame."""
-    print(volunteers_dataframe)
-    print()
-    print("General Debugging information about volunteers_dataframe")
-    print(volunteers_dataframe.columns)
-    print(volunteers_dataframe.columns[0])
-    print(volunteers_dataframe.dtypes)
-    print()
-    print("Debugging information about volunteers_dataframe's size")
-    print(volunteers_dataframe.ndim)
-    print(volunteers_dataframe.shape)
-    print(volunteers_dataframe.size)
-    print(volunteers_dataframe.memory_usage())
-    print()
+    logger = configure.configure_logging()
+    logger.debug(volunteers_dataframe)
+    logger.debug("General Debugging information about volunteers_dataframe")
+    logger.debug(volunteers_dataframe.columns)
+    logger.debug(volunteers_dataframe.columns[0])
+    logger.debug(volunteers_dataframe.dtypes)
+    logger.debug("Debugging information about volunteers_dataframe's size")
+    logger.debug(volunteers_dataframe.ndim)
+    logger.debug(volunteers_dataframe.shape)
+    logger.debug(volunteers_dataframe.size)
+    logger.debug(volunteers_dataframe.memory_usage())
     # display the shifts for a specified person
     greg_shifts = volunteers_dataframe[volunteers_dataframe['Volunteer Name'] == "Gregory Kapfhammer"]
-    print()
-    print(greg_shifts)
-    print()
+    logger.debug(greg_shifts)
     greg_shifts_list = []
     for column_name, column_contents in greg_shifts.iteritems():
-        print(f"Column Name:  {column_name}")
-        print(f"Column Contents:  {column_contents.values}")
-        print(f"Column Contents Type:  {type(column_contents.values)}")
-        print(f"Column Contents array index:  {column_contents.values[0]}")
-        print(f"Column Contents array index type:  {type(column_contents.values[0])}")
+        logger.debug(f"Column Name:  {column_name}")
+        logger.debug(f"Column Contents:  {column_contents.values}")
+        logger.debug(f"Column Contents Type:  {type(column_contents.values)}")
+        logger.debug(f"Column Contents array index:  {column_contents.values[0]}")
+        logger.debug(f"Column Contents array index type:  {type(column_contents.values[0])}")
         if column_contents.values[0] is numpy.bool_(True):
-            print("Working the shift!")
+            logger.debug("Working the shift!")
             greg_shifts_list.append(column_name)
-    print(greg_shifts_list)
-    print()
+    logger.debug(greg_shifts_list)
