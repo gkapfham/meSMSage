23906c0b3e3ada9c6e216e09616b9570b5f1bcbb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-03-31 13:07:53 -0400

    feat: Add comments and better logging configuration in configure module.

--- meSMSage/meSMSage/configure.py
+++ meSMSage/meSMSage/configure.py
@@ -6,7 +6,16 @@ import logging.config
 from rich.logging import RichHandler
 from rich.traceback import install
 
+# The defined levels, in order of increasing severity, are as follows:
+#
+# DEBUG
+# INFO
+# WARNING
+# ERROR
+# CRITICAL
+
 DEFAULT_LOGGING_LEVEL = "DEBUG"
+
 FORMAT = "%(message)s"
 RICH = "rich"
 
@@ -18,12 +27,13 @@ def configure_tracebacks() -> None:
     install()
 
 
-def configure_logging(debug_level: str = DEFAULT_LOGGING_LEVEL) -> logging.Logger:
+def configure_logging(debug_level: str = DEFAULT_LOGGING_LEVEL, force: bool = False) -> logging.Logger:
     """Configure standard Python logging package to use rich."""
     logging.basicConfig(
         level=debug_level, format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
     )
+    # if the global logger has not yet been set, set it to the configured logger
     global logger
-    if logger is None:
+    if logger is None or force is True:
         logger = logging.getLogger(RICH)
     return logger
