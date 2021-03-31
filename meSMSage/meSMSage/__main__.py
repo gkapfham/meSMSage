ed57f0663fc77e4341de79fb817f7c9d500fc3be
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-03-31 12:16:24 -0400

    refactor: Remove the use of gspread from the __main__ module.

--- meSMSage/meSMSage/__main__.py
+++ meSMSage/meSMSage/__main__.py
@@ -1,25 +1,32 @@
 """Define the command-line interface for the meSMSage program."""
 
 import demonstrate
-import sheet
+import sheets
+
+from rich.traceback import install
 
 import pandas
 import typer
 
 
 def main(
-    googlesheet: str = typer.Option(...),
+    googlesheet_id: str = typer.Option(...),
 ):
     """Access the command-line argument(s) and then perform actions."""
+    # setup rich-based tracebacks to enable better debugging on program crash
+    install()
     # display the debugging output for the program's command-line arguments
     typer.echo("")
-    typer.echo(f"The chosen Google Sheet is {sheet}!")
+    typer.echo(f"The chosen Google Sheet is {googlesheet_id}!")
     typer.echo("")
-    # create a connection to the specified Google Sheet
-    worksheet = sheet.connect(googlesheet)
-    # create a Pandas volunteers_dataframe from the spreadsheet's worksheet
-    volunteers_dataframe = pandas.DataFrame(worksheet.get_all_records())
-    demonstrate.demonstrate_pandas_analysis(volunteers_dataframe)
+    # connect the specified Google Sheet using the default internal sheet of "Sheet1"
+    sheet = sheets.connect_to_sheet(googlesheet_id)
+    data_range = sheet.get_data_range()
+    values = data_range.get_values()
+    print(f"All values: {values}")
+    sheetfu_volunteers_dataframe = pandas.DataFrame(values[1:len(values)], columns=values[0])
+    print(sheetfu_volunteers_dataframe)
+    demonstrate.demonstrate_pandas_analysis(sheetfu_volunteers_dataframe)
 
 
 if __name__ == "__main__":
