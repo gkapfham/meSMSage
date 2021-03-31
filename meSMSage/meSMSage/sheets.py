053b2a421f202e2dcb4a721ade56f7cac6fb9502
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-03-31 12:19:40 -0400

    refactor: Remove the use of the decouple package in the sheets module.

--- meSMSage/meSMSage/sheets.py
+++ meSMSage/meSMSage/sheets.py
@@ -1,17 +1,18 @@
 """Connect to a Google Sheet using gspread to access sheets at data frames."""
 
 from dotenv import load_dotenv
+from sheetfu import model
 from sheetfu import SpreadsheetApp
 
-from decouple import config
 
 SHEET = "Sheet1"
 
 
-def connect_to_sheet(requested_spreadsheet_id: str, requested_sheet_name: str = SHEET):
+def connect_to_sheet(requested_spreadsheet_id: str, requested_sheet_name: str = SHEET) -> model.Sheet:
     """Connect to the specified Google Sheet and return the requested sheet (default is "Sheet1")."""
     load_dotenv()
     sa = SpreadsheetApp(from_env=True)
     spreadsheet = sa.open_by_id(requested_spreadsheet_id)
     sheet = spreadsheet.get_sheet_by_name(requested_sheet_name)
+    print(type(sheet))
     return sheet
