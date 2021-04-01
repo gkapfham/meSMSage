c764ee794effe585345774f7c0e9b666214b94b2
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-01 17:30:03 -0400

    feat: Add code to main that creates .env file name and loads earlier.

--- meSMSage/mesmsage/main.py
+++ meSMSage/mesmsage/main.py
@@ -1,12 +1,16 @@
 """Define the command-line interface for the meSMSage program."""
 
+import os
+
 from enum import Enum
 from pathlib import Path
 
 
 from mesmsage import configure
+from mesmsage import constants
 from mesmsage import demonstrate
 from mesmsage import sheets
+from mesmsage import util
 
 import typer
 
@@ -32,8 +36,8 @@ def history():
 @app.command()
 def send(
     googlesheet_id: str = typer.Option(...),
-    env_file: Path = typer.Option(...),
     debug_level: DebugLevel = DebugLevel.DEBUG,
+    env_file: Path = typer.Option(None),
 ):
     """Send SMS messages."""
     # configure the use of rich for improved terminal output
@@ -45,14 +49,19 @@ def send(
     logger.debug(f"The Google Sheet is {googlesheet_id}.")
     logger.debug(f"The debugging level is {debug_level.value}.")
     # construct the full name of the .env file
-    if env_file is None:
-        typer.echo("No data file specified!")
-        raise typer.Abort()
+    env_file_name = ""
     # the file was specified and it is valid so derive its full name
-    if env_file.is_file():
-        env_file_name = env_file.parent.name + env_file.anchor + env_file.name
+    if env_file is not None:
+        if env_file.is_file():
+            env_file_name = env_file.parent.name + env_file.anchor + env_file.name
+    # the file name was not specified so construct the default name
+    else:
+        env_file_name = constants.markers.Nothing.join([os.getcwd(), os.sep, ".env"])
+    logger.debug(f"Environment file: {env_file_name}")
+    # load the required secure environment for connecting to Google Sheets
+    util.load_environment(env_file_name)
     # connect the specified Google Sheet using the default internal sheet of "Sheet1"
-    sheet = sheets.connect_to_sheet(googlesheet_id, env_file_name)
+    sheet = sheets.connect_to_sheet(googlesheet_id)
     # extract the Pandas data frame from the sheet in sheetfu's internal format
     dataframe = sheets.extract_dataframe(sheet)
     # demonstrate the use of the dataframe with an example
