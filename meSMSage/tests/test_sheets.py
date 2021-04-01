720995d648241476395333bee9ee48d2d954e938
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-01 17:32:50 -0400

    test: Load default environ (use default .env file) in test_sheets.

--- meSMSage/tests/test_sheets.py
+++ meSMSage/tests/test_sheets.py
@@ -1,16 +1,17 @@
 """Test the functions in the sheets module."""
 
+from googleapiclient import errors
 from sheetfu import model
 
-from googleapiclient import errors
-
 from mesmsage import sheets
+from mesmsage import util
 
 import pytest
 
 
 def test_connect_to_existing_sheet():
     """Ensure that a worksheet is returned for an existing sheet."""
+    util.load_environment()
     googlesheet_id_exists = "1ujmuIJy1NPhasIF4tnRSae0033hwa9g0wul5Ii2NwZU"
     sheet = sheets.connect_to_sheet(googlesheet_id_exists)
     assert sheet is not None
@@ -20,6 +21,7 @@ def test_connect_to_existing_sheet():
 def test_connect_to_not_existing_sheet():
     """Ensure that a worksheet is not returned for a not existing sheet."""
     with pytest.raises(errors.HttpError):
+        util.load_environment()
         googlesheet_id_exists = "1ujmuIJy1NPhasIF4tnRSae0033hwa9g0wul5Ii2NwZU-WRONG"
         sheet = sheets.connect_to_sheet(googlesheet_id_exists)
         assert sheet is not None
