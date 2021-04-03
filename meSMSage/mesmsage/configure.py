5043e9e127ea4a885f91eba1098a938225b33189
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-03 17:08:49 -0400

    refactor: Remove the constants from the configure module.

--- meSMSage/mesmsage/configure.py
+++ meSMSage/mesmsage/configure.py
@@ -6,18 +6,7 @@ import logging.config
 from rich.logging import RichHandler
 from rich.traceback import install
 
-# The defined levels, in order of increasing severity, are as follows:
-#
-# DEBUG
-# INFO
-# WARNING
-# ERROR
-# CRITICAL
-
-DEFAULT_LOGGING_LEVEL = "DEBUG"
-
-FORMAT = "%(message)s"
-RICH = "rich"
+from mesmsage import constants
 
 logger = None
 
@@ -28,14 +17,14 @@ def configure_tracebacks() -> None:
 
 
 def configure_logging(
-    debug_level: str = DEFAULT_LOGGING_LEVEL, force: bool = False
+    debug_level: str = constants.logging.Default_Logging_Level, force: bool = False
 ) -> logging.Logger:
     """Configure standard Python logging package to use rich."""
     logging.basicConfig(
-        level=debug_level, format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+        level=debug_level, format=constants.logging.Format, datefmt="[%X]", handlers=[RichHandler()]
     )
     # if the global logger has not yet been set, set it to the configured logger
     global logger
     if logger is None or force is True:
-        logger = logging.getLogger(RICH)
+        logger = logging.getLogger(constants.logging.Rich)
     return logger
