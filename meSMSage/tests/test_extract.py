f24ca68a27b928757c084782cefa1861ee550075
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-03 17:37:06 -0400

    test: Change the name of the columns in the test_extract test suite.

--- meSMSage/tests/test_extract.py
+++ meSMSage/tests/test_extract.py
@@ -9,8 +9,8 @@ def test_extract_individual_names():
     """Ensure that it is possible to extract the names of individuals."""
     dataframe = pandas.DataFrame(
         {
-            "Volunteer Name": ["Gregory", "Jessica", "Madelyn"],
-            "Volunteer Phone Number": ["888-111-5555", "888-222-5555", "888-333-5555"],
+            "Individual Name": ["Gregory", "Jessica", "Madelyn"],
+            "Individual Phone Number": ["888-111-5555", "888-222-5555", "888-333-5555"],
         }
     )
     individual_names = extract.extract_individual_names(dataframe)
