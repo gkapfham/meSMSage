9392fe10097950963fbba20fbb9077c6d97eef6a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-03 18:51:28 -0400

    feat: Change name of function and rewrite code in extract module.

--- meSMSage/mesmsage/extract.py
+++ meSMSage/mesmsage/extract.py
@@ -9,12 +9,12 @@ import pandas
 from mesmsage import constants
 
 
-def extract_individual_names(
-    volunteers_dataframe: pandas.DataFrame,
+def get_individual_names(
+    individuals_dataframe: pandas.DataFrame,
 ) -> pandas.core.series.Series:
     """Extract the names of individuals from the data frame."""
     logger = logging.getLogger(constants.logging.Rich)
-    individuals = volunteers_dataframe.loc[:, constants.sheets.Name]
+    individuals = individuals_dataframe.loc[:, constants.sheets.Name]
     logger.debug(individuals)
     return individuals
 
