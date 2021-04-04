5e0c732f9997efba5da33837b31a7fa11b8a4166
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-03 22:57:44 -0400

    feat: Add the progress and size constants to constants module.

--- meSMSage/mesmsage/constants.py
+++ meSMSage/mesmsage/constants.py
@@ -15,6 +15,12 @@ def create_constants(name, *args, **kwargs):
     return new_constants(*itertools.chain(args, kwargs.values()))
 
 
+# define the file constants
+file = create_constants(
+    "file",
+    Env=".env",
+)
+
 # The defined levels, in order of increasing severity, are as follows:
 #
 # DEBUG
@@ -41,6 +47,12 @@ markers = create_constants(
     Nothing="",
     Space=" ",
 )
+
+# define constants for the progress bars
+progress = create_constants("progress", Small_Step=0.2, Medium_Step=0.4, Large_Step=0.6)
+
+# define constants for the various sizes
+size = create_constants("size", Tab=4)
 
 # define the terminology used for spreadsheets
 sheets = create_constants(
