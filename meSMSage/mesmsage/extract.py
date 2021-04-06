62b0acdbc72d65f67d1f580835d7b3558b6c4507
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-05 22:30:14 -0400

    feat: Add the get_individual_numbers to the extract module.

--- meSMSage/mesmsage/extract.py
+++ meSMSage/mesmsage/extract.py
@@ -28,6 +28,19 @@ def get_individual_names(
     return individuals
 
 
+def get_individual_numbers(
+    individuals_dataframe: pandas.DataFrame, chosen_individuals_list: List[str]
+) -> pandas.DataFrame:
+    """Get the phone numbers for each of the specified individuals in a complete data frame with names."""
+    logger = logging.getLogger(constants.logging.Rich)
+    phone_numbers = individuals_dataframe.loc[
+        individuals_dataframe["Individual Name"].isin(chosen_individuals_list),
+        ["Individual Name", "Individual Phone Number"],
+    ]
+    logger.debug(f"Phone numbers: {phone_numbers}")
+    return phone_numbers
+
+
 def convert_series_to_list(series: pandas.core.series.Series) -> List:
     """Convert a pandas series to a standard list."""
     series_list = series.values.tolist()
