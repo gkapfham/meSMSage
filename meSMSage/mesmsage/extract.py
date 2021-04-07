36b595913d16e303bc8b8070279b0024d7363a94
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-06 21:07:54 -0400

    feat: Add a draft of name extraction and activities in extract module.

--- meSMSage/mesmsage/extract.py
+++ meSMSage/mesmsage/extract.py
@@ -1,7 +1,9 @@
 """Extract contents from a Pandas dataframe."""
 
+import itertools
 import logging
 
+from typing import Dict
 from typing import List
 
 import pandas
@@ -39,6 +41,52 @@ def get_individual_numbers(
     ]
     logger.debug(f"Phone numbers: {phone_numbers}")
     return phone_numbers
+
+
+def get_individual_activities(
+    individuals_dataframe: pandas.DataFrame, chosen_individuals_list: List[str]
+) -> Dict[str, str]:
+    """Get the activities for each of the specified individuals."""
+    pandas.set_option("display.max_columns", None)
+    logger = logging.getLogger(constants.logging.Rich)
+    individuals_have_shifts = individuals_dataframe[individuals_dataframe.any(1)]
+    specific_individuals_have_shifts = individuals_have_shifts.loc[
+        individuals_have_shifts["Individual Name"].isin(chosen_individuals_list)
+    ]
+    logger.debug(specific_individuals_have_shifts)
+    specific_individuals_specific_shifts = specific_individuals_have_shifts.loc[
+        :, (specific_individuals_have_shifts != 0).any(axis=0)
+    ]
+    logger.debug(specific_individuals_specific_shifts)
+    specific_individuals_specific_shifts_dict = (
+        specific_individuals_specific_shifts.to_dict("list")
+    )
+    specific_individuals_specific_shifts_tall = pandas.DataFrame.from_dict(
+        specific_individuals_specific_shifts_dict, orient="index"
+    )
+    name_activities_dictionary = {}
+    for (
+        column_name,
+        column_contents,
+    ) in specific_individuals_specific_shifts_tall.iteritems():
+        logger.debug(list(column_contents))
+        logger.debug(list(column_contents.index.values))
+        current_name = None
+        for (data, metadata) in zip(
+            list(column_contents), list(column_contents.index.values)
+        ):
+            logger.debug(f"Data: {data}")
+            logger.debug(f"Metadata: {metadata}")
+            if metadata == "Individual Name":
+                current_name = data
+            if data is True:
+                if current_name in name_activities_dictionary:
+                    metadata_list = name_activities_dictionary[current_name]
+                    metadata_list.append(metadata)
+                else:
+                    name_activities_dictionary[current_name] = [metadata]
+        current_name = None
+    return name_activities_dictionary
 
 
 def convert_series_to_list(series: pandas.core.series.Series) -> List:
