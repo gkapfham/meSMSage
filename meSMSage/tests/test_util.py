6bd5f8a72ce634188a7021444b8ba6286afb0580
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-08 16:01:48 -0400

    test: Add test cases to test_util for the reindent function.

--- meSMSage/tests/test_util.py
+++ meSMSage/tests/test_util.py
@@ -11,3 +11,19 @@ def test_dotenv_creates_environment():
     key = "SHEETFU_CONFIG_TYPE"
     value = os.getenv(key)
     assert value == "service_account"
+
+
+def test_reindent_short_string():
+    """Ensure that a short re-indented string exists after function call."""
+    message = "hello"
+    reindented_message = util.reindent(message)
+    assert reindented_message is not None
+    assert reindented_message == "    hello"
+
+
+def test_reindent_two_word_string():
+    """Ensure that a short two-word re-indented string exists after function call."""
+    message = "hello world"
+    reindented_message = util.reindent(message)
+    assert reindented_message is not None
+    assert reindented_message == "    hello world"
