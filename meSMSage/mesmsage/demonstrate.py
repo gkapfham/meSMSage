81ff772f2e3790812cf027ce19dd6a716ee50fd8
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-08 11:47:33 -0400

    refactor: Comment out a lot of not-needed code in demonstrate module.

--- meSMSage/mesmsage/demonstrate.py
+++ meSMSage/mesmsage/demonstrate.py
@@ -43,34 +43,35 @@ def demonstrate_pandas_analysis(volunteers_dataframe: pandas.DataFrame) -> None:
             logger.debug("Working the shift!")
             greg_shifts_list.append(column_name)
     logger.debug(greg_shifts_list)
-    data = volunteers_dataframe
-    names = ["Gregory Kapfhammer", "Jessica Kapfhammer", "Jennifer Dingeldine"]
-    phone_numbers = data.loc[data["Individual Name"].isin(names)]
-    logger.debug(phone_numbers)
-    logger.debug(type(phone_numbers))
-    output = phone_numbers[phone_numbers.eq(True).any(axis=1)]
-    logger.debug(output.head())
-    matching = phone_numbers.loc[:, (phone_numbers == True).any()]
-    again = output.loc[(output["Individual Name"].isin(names)) | (output == True).any()]
-    logger.debug(again)
-    logger.debug(matching)
-    has_shift = data[data.any(1)]  # GOOD
-    has_shift_specific = has_shift.loc[has_shift["Individual Name"].isin(names)]  # GOOD
-    logger.debug(has_shift_specific)
-    hsss = has_shift_specific.loc[:, (has_shift_specific != 0).any(axis=0)]  # GOOD
-    logger.debug(hsss)
-    for column_name, column_contents in hsss.iteritems():
-        if column_name == "Individual Name":
-            logger.debug(f"The name is {column_contents.values}")
-    for index, row in hsss.iterrows():
-        logger.debug(row["Individual Name"])
-        logger.debug(row["Individual Phone Number"])
-    dictionary = hsss.to_dict("list")
-    logger.debug(dictionary)
-    tryit_long = pandas.DataFrame.from_dict(dictionary)
-    logger.debug(tryit_long)
-    tryit_tall = pandas.DataFrame.from_dict(dictionary, orient="index")  # GOOD
-    logger.debug(tryit_tall)  # GOOD
-    for column_name, column_contents in tryit_tall.iteritems():
-        logger.debug(list(column_contents))
-        logger.debug(list(column_contents.index.values))
+
+    # data = volunteers_dataframe
+    # names = ["Gregory Kapfhammer", "Jessica Kapfhammer", "Jennifer Dingeldine"]
+    # phone_numbers = data.loc[data["Individual Name"].isin(names)]
+    # logger.debug(phone_numbers)
+    # logger.debug(type(phone_numbers))
+    # output = phone_numbers[phone_numbers.eq(True).any(axis=1)]
+    # logger.debug(output.head())
+    # matching = phone_numbers.loc[:, (phone_numbers == True).any()]
+    # again = output.loc[(output["Individual Name"].isin(names)) | (output == True).any()]
+    # logger.debug(again)
+    # logger.debug(matching)
+    # has_shift = data[data.any(1)]  # GOOD
+    # has_shift_specific = has_shift.loc[has_shift["Individual Name"].isin(names)]  # GOOD
+    # logger.debug(has_shift_specific)
+    # hsss = has_shift_specific.loc[:, (has_shift_specific != 0).any(axis=0)]  # GOOD
+    # logger.debug(hsss)
+    # for column_name, column_contents in hsss.iteritems():
+    #     if column_name == "Individual Name":
+    #         logger.debug(f"The name is {column_contents.values}")
+    # for index, row in hsss.iterrows():
+    #     logger.debug(row["Individual Name"])
+    #     logger.debug(row["Individual Phone Number"])
+    # dictionary = hsss.to_dict("list")
+    # logger.debug(dictionary)
+    # tryit_long = pandas.DataFrame.from_dict(dictionary)
+    # logger.debug(tryit_long)
+    # tryit_tall = pandas.DataFrame.from_dict(dictionary, orient="index")  # GOOD
+    # logger.debug(tryit_tall)  # GOOD
+    # for column_name, column_contents in tryit_tall.iteritems():
+    #     logger.debug(list(column_contents))
+    #     logger.debug(list(column_contents.index.values))
