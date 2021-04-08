2d530625cf1f9c0d538733d609d3f08ae5dac403
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-08 12:51:38 -0400

    test: Add "googlesheets" marker and reorder imports in test_sheets.

--- meSMSage/tests/test_sheets.py
+++ meSMSage/tests/test_sheets.py
@@ -1,5 +1,7 @@
 """Test the functions in the sheets module."""
 
+import pytest
+
 from googleapiclient import errors
 from sheetfu import model
 from sheetfu.exceptions import SheetNameNoMatchError
@@ -8,9 +10,8 @@ from mesmsage import sheets
 from mesmsage import util
 from mesmsage.sheets import SheetNotFoundError
 
-import pytest
 
-
+@pytest.mark.googlesheets
 def test_connect_to_existing_googlesheet():
     """Ensure that a worksheet is returned for an existing sheet."""
     util.load_environment()
@@ -20,6 +21,7 @@ def test_connect_to_existing_googlesheet():
     assert type(sheet) is model.Sheet
 
 
+@pytest.mark.googlesheets
 def test_connect_to_not_existing_googlesheet():
     """Ensure that a worksheet is not returned for a not-existing Google sheet."""
     with pytest.raises(errors.HttpError):
@@ -28,6 +30,7 @@ def test_connect_to_not_existing_googlesheet():
         _ = sheets.connect_to_sheet(googlesheet_id_exists)
 
 
+@pytest.mark.googlesheets
 def test_connect_to_not_existing_worksheet_in_valid_googlesheet():
     """Ensure that a sheet is not returned for a not-existing worksheet in a valid Google sheet."""
     with pytest.raises(SheetNameNoMatchError):
@@ -38,6 +41,7 @@ def test_connect_to_not_existing_worksheet_in_valid_googlesheet():
         )
 
 
+@pytest.mark.googlesheets
 def test_extract_dataframe_from_existing_googlesheet_and_existing_worksheet():
     """Ensure that it is possible to extract a Pandas dataframe from a Sheetfu sheet."""
     util.load_environment()
