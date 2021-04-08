a52ddc17abf3fdc6c35491e8e17ef3d08554fc7e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-08 13:52:00 -0400

    feat: Add more functions to util for manipulating textual content.

--- meSMSage/mesmsage/util.py
+++ meSMSage/mesmsage/util.py
@@ -1,6 +1,12 @@
-"""Utility functions for manipulating the environment."""
+"""Utility functions for manipulating the environment and textual content."""
+
+from textwrap import indent
+from typing import Dict
+from typing import List
 
 from dotenv import load_dotenv
+
+from mesmsage import constants
 
 
 def load_environment(env_file_name: str = None) -> None:
@@ -12,3 +18,16 @@ def load_environment(env_file_name: str = None) -> None:
     # --> file is specified, so load from it instead of environment variables
     else:
         load_dotenv(dotenv_path=env_file_name)
+
+
+def get_printable_dictionary(provided_dict: Dict[str, List[str]]) -> str:
+    """Create a textual representation of a dictionary."""
+    lines = []
+    for name, activities in provided_dict.items():
+        lines.append(name + " -> " + ", ".join(activities))
+    return reindent("\n".join(lines), constants.sizes.Tab)
+
+
+def reindent(text: str, num_spaces: int = 4) -> str:
+    """Add indentation spaces to a (potentially) multiline string."""
+    return indent(text, constants.markers.Space * num_spaces)
