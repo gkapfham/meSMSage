019210106b15769abc4d55b937c2f931058ad04a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-09 15:25:12 -0400

    test: Add more test cases to test_constants for more constants.

--- meSMSage/tests/test_constants.py
+++ meSMSage/tests/test_constants.py
@@ -16,3 +16,25 @@ def test_environment_constant_cannot_redefine():
     """Check cannot redefine the variables in the environment constant."""
     with pytest.raises(AttributeError):
         constants.environment.Twilio_Phone_Number = CANNOT_SET_CONSTANT_VARIABLE
+
+
+def test_files_constant_defined():
+    """Check correctness for the variables in the environment constant."""
+    assert constants.files.Env == ".env"
+
+
+def test_files_constant_cannot_redefine():
+    """Check cannot redefine the variables in the environment constant."""
+    with pytest.raises(AttributeError):
+        constants.files.Env = CANNOT_SET_CONSTANT_VARIABLE
+
+
+def test_locations_constant_defined():
+    """Check correctness for the variables in the environment constant."""
+    assert constants.locations.Us == "US"
+
+
+def test_locations_constant_cannot_redefine():
+    """Check cannot redefine the variables in the environment constant."""
+    with pytest.raises(AttributeError):
+        constants.locations.Us = CANNOT_SET_CONSTANT_VARIABLE
