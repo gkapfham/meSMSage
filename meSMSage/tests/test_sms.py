15a6ac2d631fa9ae9fd012df6b7a37c1bd10b3d2
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-10 14:35:24 -0400

    test: Add tests to test_sms for both the internal and external client.

--- meSMSage/tests/test_sms.py
+++ meSMSage/tests/test_sms.py
@@ -4,18 +4,35 @@ import os
 
 import pytest
 
+from unittest import mock
+
 from twilio.rest import Client
 
 from mesmsage import constants
 from mesmsage import sms
+from mesmsage.sms import client
 from mesmsage import util
 
 
 @pytest.mark.twilio
-def test_send_single_message_real_twilio():
-    """Ensure that a worksheet is returned for an existing sheet."""
+def test_send_single_message_real_twilio_constructed_client():
+    """Ensure that it is possible to send the SMS message through Twilio service."""
     util.load_environment()
-    client = Client()
+    internal_client = Client()
+    twilio_phone_number = os.getenv(constants.environment.Twilio_Phone_Number)
+    recipient_phone_number = os.getenv(constants.environment.Recipient_Phone_Number)
+    sid = sms.send_message(
+        internal_client,
+        recipient_phone_number,
+        twilio_phone_number,
+        "Test: Sending Message Through Twilio",
+    )
+    assert sid is not None
+
+
+@pytest.mark.twilio
+def test_send_single_message_real_twilio_global_client():
+    """Ensure that it is possible to send the SMS message through Twilio service."""
     twilio_phone_number = os.getenv(constants.environment.Twilio_Phone_Number)
     recipient_phone_number = os.getenv(constants.environment.Recipient_Phone_Number)
     sid = sms.send_message(
@@ -25,3 +42,21 @@ def test_send_single_message_real_twilio():
         "Test: Sending Message Through Twilio",
     )
     assert sid is not None
+
+
+@mock.patch('mesmsage.sms.client.messages.create')
+def test_send_single_message_mock_twilio(create_message_mock):
+    """Ensure that it is possible to send the SMS message through a mocked Twilio service."""
+    twilio_phone_number = os.getenv(constants.environment.Twilio_Phone_Number)
+    recipient_phone_number = os.getenv(constants.environment.Recipient_Phone_Number)
+    expected_sid = "SM87105da94bff44b999e4e6eb90d8eb6a"
+    create_message_mock.return_value.sid = expected_sid
+    sid = sms.send_message(
+        client,
+        recipient_phone_number,
+        twilio_phone_number,
+        "Test: Sending Message Through Twilio",
+    )
+    assert create_message_mock.called is True
+    assert sid is not None
+    assert sid == expected_sid
