a6abfe09083357d305ef65e43002a7e1defcf442
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-10 13:41:41 -0400

    test: Add more test assertions for new constants in test_constants.

--- meSMSage/tests/test_constants.py
+++ meSMSage/tests/test_constants.py
@@ -23,11 +23,14 @@ def test_dataframes_constant_cannot_redefine():
 
 def test_environment_constant_defined():
     """Check correctness for the variables in the environment constant."""
+    assert constants.environment.Recipient_Phone_Number == "RECIPIENT_PHONE_NUMBER"
     assert constants.environment.Twilio_Phone_Number == "TWILIO_PHONE_NUMBER"
 
 
 def test_environment_constant_cannot_redefine():
     """Check cannot redefine the variables in the environment constant."""
+    with pytest.raises(AttributeError):
+        constants.environment.Recipient_Phone_Number = CANNOT_SET_CONSTANT_VARIABLE
     with pytest.raises(AttributeError):
         constants.environment.Twilio_Phone_Number = CANNOT_SET_CONSTANT_VARIABLE
 
