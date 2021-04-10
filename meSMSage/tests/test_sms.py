3261c566170bff7fad4ee6fcdc728da26cb151cb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-10 15:28:38 -0400

    test: Add test case for checking exceptions in test_sms test suite.

--- meSMSage/tests/test_sms.py
+++ meSMSage/tests/test_sms.py
@@ -6,6 +6,7 @@ import pytest
 
 from unittest import mock
 
+from twilio.base.exceptions import TwilioRestException
 from twilio.rest import Client
 
 from mesmsage import constants
@@ -44,7 +45,7 @@ def test_send_single_message_real_twilio_global_client():
     assert sid is not None
 
 
-@mock.patch('mesmsage.sms.client.messages.create')
+@mock.patch("mesmsage.sms.client.messages.create")
 def test_send_single_message_mock_twilio(create_message_mock):
     """Ensure that it is possible to send the SMS message through a mocked Twilio service."""
     twilio_phone_number = os.getenv(constants.environment.Twilio_Phone_Number)
@@ -60,3 +61,25 @@ def test_send_single_message_mock_twilio(create_message_mock):
     assert create_message_mock.called is True
     assert sid is not None
     assert sid == expected_sid
+
+
+@mock.patch("mesmsage.sms.client.messages.create")
+def test_send_single_message_mock_twilio_exception(create_message_mock, caplog):
+    """Ensure that incorrectly sending the SMS message through a mocked Twilio service will create logging output."""
+    twilio_phone_number = os.getenv(constants.environment.Twilio_Phone_Number)
+    recipient_phone_number = os.getenv(constants.environment.Recipient_Phone_Number)
+    error_message = f"Unable to create record: The 'To' number {twilio_phone_number} is not a valid phone number."
+    status = 500
+    uri = "/Accounts/ACXXXXXXXXXXXXXXXXX/Messages.json"
+    create_message_mock.side_effect = TwilioRestException(
+        status, uri, msg=error_message
+    )
+    sid = sms.send_message(
+        client,
+        recipient_phone_number,
+        twilio_phone_number,
+        "Test: Sending Message Through Twilio",
+    )
+    assert sid is None
+    assert error_message in caplog.text
+    assert constants.messages.Sms_Did_Not_Work in caplog.text
