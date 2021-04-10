8f1e122986e6a2f82c766b2ebdcd9b32ce6e1149
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-04-10 15:34:13 -0400

    refactor: Correctly use constants in send_message of the sms module.

--- meSMSage/mesmsage/sms.py
+++ meSMSage/mesmsage/sms.py
@@ -35,7 +35,7 @@ def send_message(client: Client, to_number: str, from_number: str, message: str)
     # something went wrong with sending the message, so log
     # it as an error that will be visible in the logging system
     except TwilioRestException as e:
-        logger.error("Sending SMS with Twilio did not work: " + str(e))
+        logger.error(constants.messages.Sms_Did_Not_Work + constants.markers.Space + str(e))
         return None
     # return the 34-character string that serves as the unique
     # identifier for this specific message sent through Twilio.
